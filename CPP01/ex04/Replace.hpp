e0d02395738c27bed5ef557225e7d0078b6caf8b
Mariia KLIMINA <mklimina@bess-f2r4s15.clusters.42paris.fr>
2024-01-22 21:11:28 +0100

    ouf

---
+++ CPP01/ex04/Replace.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Replace.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mklimina <mklimina@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/22 19:55:25 by mklimina          #+#    #+#             */
+/*   Updated: 2024/01/22 20:11:51 by mklimina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WEAPON_HPP
+#define WEAPON_HPP
+#include <iostream>
+# include <iostream>
+# include <fstream>
+# include <string>
+
+class Replacer 
+{
+	private:
+		std::string input_file;
+		std::string output_file;
+
+	public:
+		std::string return_replaced_value();
+		void replace_();
+		
+		
+		
+		
+}
+;
+#endif
