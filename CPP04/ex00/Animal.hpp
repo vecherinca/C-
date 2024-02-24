e3a7486d35be9371bfac14a3d39d4a66a0b850da
Mariia KLIMINA <mklimina@bess-f2r4s15.clusters.42paris.fr>
2024-02-24 16:44:05 +0100

    lol

---
+++ CPP04/ex00/Animal.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mklimina <mklimina@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/22 19:52:37 by mklimina          #+#    #+#             */
+/*   Updated: 2024/02/22 19:54:27 by mklimina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+#define ANIMAL_HPP
+
+#include <iostream>
+class Animal
+{
+	private:
+		std::string type;
+	public:	
+};
+
+#endif
---
+++ CPP04/ex00/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mklimina <mklimina@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/16 20:52:19 by mklimina          #+#    #+#             */
+/*   Updated: 2024/02/22 19:51:53 by mklimina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+int main()
+{
+const Animal* meta = new Animal();
+const Animal* j = new Dog();
+const Animal* i = new Cat();
+std::cout << j->getType() << " " << std::endl;
+std::cout << i->getType() << " " << std::endl;
+i->makeSound(); //will output the cat sound!
+j->makeSound();
+meta->makeSound();
+return 0;
+}
